47adaf1be4cacff1f45850f6f10705b0851fb67e
Maks <brhumaan@gmail.com>
2026-03-12 13:05:54 +0200

    feat: add preloader

--- src/main.js
+++ src/main.js
@@ -466,6 +466,17 @@ class Game {
 
         this.buildMaze();
         this.animate();
+        
+        // Hide preloader after game initialization
+        setTimeout(() => {
+            const preloader = document.getElementById('preloader');
+            if (preloader) {
+                preloader.style.animation = 'fadeOut 1s ease-in-out';
+                setTimeout(() => {
+                    preloader.style.display = 'none';
+                }, 1000);
+            }
+        }, 2000);
     }
 
     clearMaze() {
@@ -886,13 +897,13 @@ class Game {
 
     onKeyDown(event) {
         switch (event.code) {
-            case 'ArrowUp':
+            // case 'ArrowUp':
             case 'KeyW': this.moveForward = true; break;
-            case 'ArrowLeft': this.rotateLeft = true; break;
+            // case 'ArrowLeft':
             case 'KeyA': this.moveLeft = true; break;
-            case 'ArrowDown':
+            // case 'ArrowDown':
             case 'KeyS': this.moveBackward = true; break;
-            case 'ArrowRight': this.rotateRight = true; break;
+            // case 'ArrowRight':
             case 'KeyD': this.moveRight = true; break;
             case 'Space': 
                 if (this.canJump) {
@@ -943,13 +954,13 @@ class Game {
 
     onKeyUp(event) {
         switch (event.code) {
-            case 'ArrowUp':
+            // case 'ArrowUp':
             case 'KeyW': this.moveForward = false; break;
-            case 'ArrowLeft': this.rotateLeft = false; break;
+            // case 'ArrowLeft':
             case 'KeyA': this.moveLeft = false; break;
-            case 'ArrowDown':
+            // case 'ArrowDown':
             case 'KeyS': this.moveBackward = false; break;
-            case 'ArrowRight': this.rotateRight = false; break;
+            // case 'ArrowRight':
             case 'KeyD': this.moveRight = false; break;
             case 'ControlLeft':
             case 'ControlRight':
